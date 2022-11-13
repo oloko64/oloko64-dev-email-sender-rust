8dff6fab9cbd7421112d02022a8aff40d1b803ac
OLoKo64 <naldoepingo1@gmail.com>
2022-11-13 02:19:45 -0300

    feat: raise sendgrid version and add sentry

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::{env, net::SocketAddr};
-
 use actix_cors::Cors;
-use actix_web::{HttpResponse, http};
-use actix_web::{post, web, App, HttpServer, Responder};
+use actix_web::{http, post, web, App, HttpResponse, HttpServer, Responder};
 use dotenvy::dotenv;
+use log::error;
 use sendgrid_thin::Sendgrid;
 use serde::{Deserialize, Serialize};
+use std::{env, net::SocketAddr};
 
 #[derive(Deserialize)]
 struct EmailBody {
@@ -14,46 +13,73 @@ struct EmailBody {
 }
 
 #[derive(Serialize)]
-struct EmailSendResponse {
-    message: String,
+struct EmailSendResponse<'a> {
+    message: &'a str,
     success: bool,
 
     #[serde(skip_serializing_if = "Option::is_none")]
-    error: Option<String>,
+    error: Option<&'a str>,
 }
 
 #[post("/send-mail")]
 async fn send_email(req_body: web::Json<EmailBody>) -> impl Responder {
-    let sendgrid_api_key = env::var("SENDGRID_API_KEY").unwrap();
-    let sendgrid = Sendgrid::new(
-        &sendgrid_api_key,
-        "reinaldorozatoj@gmail.com",
-        "reinaldorozatoj.11cg1@aleeas.com",
-    );
+    let sendgrid_api_key = match env::var("SENDGRID_API_KEY") {
+        Ok(api_key) => api_key,
+        Err(err) => {
+            sentry::capture_error(&err);
+            return HttpResponse::InternalServerError().json(EmailSendResponse {
+                message: "Internal Server Error",
+                success: false,
+                error: Some("Sendgrid API Key not found"),
+            });
+        }
+    };
+    let mut sendgrid = Sendgrid::new(&sendgrid_api_key);
+    sendgrid
+        .set_from_email("reinaldorozatoj.11cg1@aleeas.com")
+        .set_to_emails(&["reinaldorozatoj@gmail.com"])
+        .set_subject(&req_body.subject)
+        .set_body(&req_body.body);
 
-    match sendgrid.send_mail(&req_body.subject, &req_body.body) {
-        Ok(_) => HttpResponse::Ok().json(EmailSendResponse {
-            message: "Email sent successfully".to_string(),
+    match sendgrid.send() {
+        Ok(message) => HttpResponse::Ok().json(EmailSendResponse {
+            message: &message,
             success: true,
             error: None,
         }),
-        Err(err) => HttpResponse::InternalServerError().json(EmailSendResponse {
-            message: "Error sending email".to_string(),
-            error: Some(err.to_string()),
-            success: false,
-        }),
+        Err(err) => {
+            sentry::capture_error(err.as_ref());
+            HttpResponse::BadRequest().json(EmailSendResponse {
+                message: "Error sending email",
+                error: Some(err.to_string().as_str()),
+                success: false,
+            })
+        }
     }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    env_logger::init();
+    let sentry_api_key = env::var("SENTRY_API_KEY").unwrap_or_else(|_| {
+        error!("Sentry API Key not found");
+        String::new()
+    });
+    let _guard = sentry::init((
+        sentry_api_key,
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            ..Default::default()
+        },
+    ));
+
     let addr = SocketAddr::from((
         [0, 0, 0, 0],
         env::var("PORT")
             .unwrap_or_else(|_| {
                 println!("PORT not found .env file, using default port: 8080");
-                "8080".to_string()
+                "8080".to_owned()
             })
             .parse::<u16>()
             .expect("Failed to parse port from .env file"),
